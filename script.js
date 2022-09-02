feeec922a40be88ba6657404a8e67eee2fa21ab1
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2022-09-02 21:56:25 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const actions = Array.from(document.querySelectorAll('[data-action]'));
+
+let counter = localStorage.getItem("counter") || 0;
+
+document.querySelector(".counter-value").innerText = counter;
+
+actions.forEach(action => {
+	action.addEventListener('click', () => {
+		const action_name = action.dataset.action;
+
+		action.classList.add("animate");
+
+		setTimeout(() => {
+			action.classList.remove("animate");
+		}, 1000);
+
+		switch (action_name) {
+			case 'increase':
+				counter++;
+				break;
+			case 'decrease':
+				counter--;
+				break;
+			case 'reset':
+				counter = 0;
+				break;
+		}
+
+		document.querySelector(".counter-value").innerText = counter;
+	});
+});
